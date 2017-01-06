40516b0914951638e82c97b652ae0a4b2833fefb
codeOfRobin <me@rmalhotra.com>
2017-01-06 18:09:47 +0530

    perfect clean room reimplementation done

--- ChatViewPrototype/ViewController.swift
+++ ChatViewPrototype/ViewController.swift
@@ -22,40 +22,22 @@ class ViewController: UIViewController, MyCustomViewDelegate {
 		let visualEffectView = UIVisualEffectView(effect: effect)
 		return visualEffectView
 	}
-
-	let textInputNode = TextBarNode()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.addSubnode(node)
 		
 		node.addSubnode(tableNode)
-		view.addSubnode(backgroundNode)
 		textInputBar.textNode.maximumLinesToDisplay = 5
 	
-		
-		backgroundNode.style.width = ASDimensionMakeWithFraction(1.0)
-		backgroundNode.style.height = ASDimensionMake(44)
 		
 		tableNode.style.preferredSize = CGSize(width: 300, height: 300)
 		tableNode.style.width = ASDimensionMakeWithFraction(1.0)
 		tableNode.style.flexGrow = 1.0
 		node.layoutSpecBlock = { size in
-			
-			let stack = ASStackLayoutSpec(direction: .vertical, spacing: 0.0, justifyContent: .center, alignItems: .stretch, children: [self.tableNode])
-			
-			let overlaySpec = ASOverlayLayoutSpec(child: self.backgroundNode, overlay: self.textInputNode)
-			
-			let relativeSpec = ASRelativeLayoutSpec(horizontalPosition: .center, verticalPosition: .end, sizingOption: [], child: overlaySpec)
-			
-			let backgroundSpec = ASBackgroundLayoutSpec(child: relativeSpec, background: stack)
-			
-			return backgroundSpec
+			return ASStackLayoutSpec(direction: .vertical, spacing: 0.0, justifyContent: .center, alignItems: .stretch, children: [self.tableNode])
 		}
 		
-		
-//		(backgroundNode.view as? UIVisualEffectView)?.contentView.addSubnode(textInputNode)
-		view.addSubnode(textInputNode)
 		
 		tableNode.dataSource = manager
 		tableNode.delegate = manager
@@ -63,14 +45,19 @@ class ViewController: UIViewController, MyCustomViewDelegate {
 		
 		tableNode.view.keyboardDismissMode = .interactive
 		
-		
 		NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil, queue: OperationQueue.main) { (notification) in
-			print(notification.userInfo)
-//			node.frame = CGRect(x: 0, y: 0, width: node.frame.width, height: )
 //			let lastRow = IndexPath(row: self.tableNode.numberOfRows(inSection: 0) - 1, section: 0)
 //			self.tableNode.scrollToRow(at: lastRow, at: .top, animated: true)
 
 		}
+		
+		
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		self.becomeFirstResponder()
+
 	}
 
 	override func viewDidLayoutSubviews() {
@@ -78,19 +65,26 @@ class ViewController: UIViewController, MyCustomViewDelegate {
 	}
 	
 	override var inputAccessoryView: UIView? {
-		let frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-		let custom = MyCustomView(frame: frame)
-		custom.backgroundColor = .red
-		custom.delegate = self
-		return custom
+		
+		guard let blurView = (backgroundNode.view as? UIVisualEffectView) else {
+			return nil
+		}
+		
+		backgroundNode.view.frame.size = CGSize(width: view.frame.width, height: 44)
+		blurView.contentView.addSubnode(textInputBar)
+		textInputBar.frame = blurView.contentView.bounds
+		return backgroundNode.view
 	}
 	
 	func centerChanged(center: CGPoint) {
-//		print(center)
 	}
 	
 	func frameChanged(frame: CGRect) {
 		node.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: frame.origin.y)
 		node.layout()
+	}
+	
+	override var canBecomeFirstResponder: Bool {
+		return true
 	}
 }
